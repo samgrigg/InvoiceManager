a5af1d7b1a53d9191410b6283a55a235161ccfe3
Sam Grigg <griggsn@ldschurch.org>
2010-11-01 22:56:19 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvoiceManager::Application.config.secret_token = 'bd089f062a02afec18ffb69c0b017f9916bb0139e036bbcb970f2778acca8e6fbc513cbae972d60e1421c4fcb9be6e1949eb1c787f59be8de40cf7f87b474829'
